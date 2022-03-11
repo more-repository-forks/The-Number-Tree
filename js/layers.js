3049549a6ed731821468a40e054586602c186024
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-03-11 12:30:40 -0500

    added gridable

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,5 @@
 addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "plains", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -7,12 +7,12 @@ addLayer("p", {
 		points: new Decimal(0),
     }},
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    requires: new Decimal(0.1), // Can be a function that takes requirement increases into account
+    resource: "basic skill", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -22,7 +22,54 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "p", description: "P: Reset for basic skill", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
-})
+    effect() {return new Decimal(1 + player['p'].points / 100)},
+    effectDescription() {return "which multiplies your basic resouce gain by " + format(tmp.p.effect)},
+    layerShown(){return true},
+    tabFormat: [
+        "main-display",
+        "prestige-button",
+        "blank",
+        ["display-text",
+            function() { return 'You have ' + format(player.points) + ' experience' },
+            {}],
+        "blank",
+        "grid"
+    ],
+    grid: {
+        rows: 6, // If these are dynamic make sure to have a max value as well!
+        cols: 6,
+        getStartData(id) {
+            if (id == 101) return "you are here"
+            if (id == 201 || id == 102) return "goable"
+            else return "empty tile"
+        },
+        getUnlocked(id) {
+            return true
+        },
+        getCanClick(data, id) {
+            if (data == "you are here" || data == "goable") return true
+            else return false
+        },
+        onClick(data, id) {
+            if (getGridData('p', id - 2) == "you are here" || getGridData('p', id - 2) == "goable") setGridData('p', id - 2, "empty tile")
+            if (getGridData('p', id + 2) == "you are here" || getGridData('p', id + 2) == "goable") setGridData('p', id + 2, "empty tile")
+            if (getGridData('p', id - 99) == "you are here" || getGridData('p', id - 99) == "goable") setGridData('p', id - 99, "empty tile")
+            if (getGridData('p', id + 99) == "you are here" || getGridData('p', id + 99) == "goable") setGridData('p', id + 99, "empty tile")
+            if (getGridData('p', id - 101) == "you are here" || getGridData('p', id - 101) == "goable") setGridData('p', id - 101, "empty tile")
+            if (getGridData('p', id + 101) == "you are here" || getGridData('p', id + 101) == "goable") setGridData('p', id + 101, "empty tile")
+            if (getGridData('p', id - 200) == "you are here" || getGridData('p', id - 200) == "goable") setGridData('p', id - 200, "empty tile")
+            if (getGridData('p', id + 200) == "you are here" || getGridData('p', id + 200) == "goable") setGridData('p', id + 200, "empty tile")
+            setGridData('p', id - 1, "goable")
+            setGridData('p', id + 1, "goable")
+            setGridData('p', id - 100, "goable")
+            setGridData('p', id + 100, "goable")
+            setGridData('p', id, "you are here")
+        },
+        getDisplay(data, id) {
+            if (data == "goable") return "empty tile"
+            return data
+        },
+    },
+});
--- js/mod.js
+++ js/mod.js
@@ -2,24 +2,24 @@ let modInfo = {
 	name: "The ??? Tree",
 	id: "mymod",
 	author: "Yrahcaz7",
-	pointsName: "points",
+	pointsName: "experience",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new Decimal (0), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Beta Test",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
+	<h3>v0.1 - Beta Test</h3><br>
+		- Changed things.<br>
 		- Added stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
@@ -42,7 +42,7 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1)
+	let gain = new Decimal(0.0001)
 	return gain
 }
 
