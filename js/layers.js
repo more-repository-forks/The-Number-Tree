5b0ee1140677e7439850b191e0a2d09a3548706d
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-03-13 11:38:32 -0400

    minor change

--- js/layers.js
+++ js/layers.js
@@ -270,7 +270,7 @@ addLayer("c", {
             {}],
         "blank",
         ["display-text",
-            function() { return 'You have ' + formatWhole(player.c.woodAxe) + ' wood axes, which each make the crafting skill effect apply to basic skill gain at a reduced rate (' + format(tmp.c.effect ** 0.05) + "x), which totals to: " + format((player.c.woodAxe * (tmp.c.effect ** 0.05 - 1)) + 1) + 'x (capped at 100x)'},
+            function() { return 'You have ' + formatWhole(player.c.woodAxe) + ' wood axes, which each make the crafting skill effect apply to basic skill gain at a reduced rate (' + format(tmp.c.effect ** 0.05) + "x), which totals to: " + format((player.c.woodAxe * (tmp.c.effect ** 0.05 - 1)) + 1) + 'x'},
             {}],
         "blank",
         "milestones",
@@ -439,9 +439,9 @@ addLayer("c", {
 });
 
 addLayer("m", {
-    name: "minigame coins", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    name: "minigame coins",
+    symbol: "M",
+    position: 1,
     startData() { return {
         unlocked: false,
 		points: new Decimal(0),
@@ -449,21 +449,21 @@ addLayer("m", {
         minigameTime: 0,
     }},
     color: "#3C82DE",
-    requires: new Decimal(25000), // Can be a function that takes requirement increases into account
-    resource: "minigame coins", // Name of prestige currency
-    baseResource: "experience", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
+    requires: new Decimal(25000),
+    resource: "minigame coins",
+    baseResource: "experience",
+    baseAmount() {return player.points},
+    type: "normal",
+    exponent: 0.5,
+    gainMult() {
         mult = new Decimal(1)
         return mult
     },
-    gainExp() { // Calculate the exponent on main currency from bonuses
+    gainExp() {
         return new Decimal(1)
     },
     canBuyMax(){return true},
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 0,
     hotkeys: [
         {key: "m", description: "M: Reset for minigame coins", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
