fe0ce7fc86dbf6f42acfc7a1515fdb01020355c9
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-05-31 07:27:20 -0400

    update version and changelog

--- js/mod.js
+++ js/mod.js
@@ -9,8 +9,8 @@ let modInfo = {
 };
 
 let VERSION = {
-	num: '1.3',
-	name: 'Greenish-Yellow?',
+	num: '2.0',
+	name: 'Gigantic Numbers',
 };
 
 let winText = '<h3>You won the game!</h3><br>However, it isn\'t the end yet...<br>Wait for more updates for further content.';
