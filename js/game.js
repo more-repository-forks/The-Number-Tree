90bbfa4d67028424eb9fcc9902f562931cc51b9d
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-28 17:45:57 -0400

    formatting

--- js/game.js
+++ js/game.js
@@ -15,20 +15,20 @@ function getResetGain(layer, useType = null) {
 			return layers[layer].getResetGain();
 	};
 	if (tmp[layer].type == "none")
-		return new Decimal (0);
+		return new Decimal(0);
 	if (tmp[layer].gainExp.eq(0)) return decimalZero;
-	if (type=="static") {
+	if (type == "static") {
 		if ((!tmp[layer].canBuyMax) || tmp[layer].baseAmount.lt(tmp[layer].requires)) return decimalOne;
 		let gain = tmp[layer].baseAmount.div(tmp[layer].requires).div(tmp[layer].gainMult).max(1).log(tmp[layer].base).times(tmp[layer].gainExp).pow(Decimal.pow(tmp[layer].exponent, -1));
 		gain = gain.times(tmp[layer].directMult);
 		return gain.floor().sub(player[layer].points).add(1).max(1);
-	} else if (type=="normal") {
+	} else if (type == "normal") {
 		if (tmp[layer].baseAmount.lt(tmp[layer].requires)) return decimalZero;
 		let gain = tmp[layer].baseAmount.div(tmp[layer].requires).pow(tmp[layer].exponent).times(tmp[layer].gainMult).pow(tmp[layer].gainExp);
 		if (gain.gte(tmp[layer].softcap)) gain = gain.pow(tmp[layer].softcapPower).times(tmp[layer].softcap.pow(decimalOne.sub(tmp[layer].softcapPower)))
 		gain = gain.times(tmp[layer].directMult);
 		return gain.floor().max(0);
-	} else if (type=="custom") {
+	} else if (type == "custom") {
 		return layers[layer].getResetGain();
 	} else {
 		return decimalZero;
@@ -46,20 +46,20 @@ function getNextAt(layer, canMax=false, useType = null) {
 		return new Decimal(Infinity);
 	if (tmp[layer].gainMult.lte(0)) return new Decimal(Infinity);
 	if (tmp[layer].gainExp.lte(0)) return new Decimal(Infinity);
-	if (type=="static") {
+	if (type == "static") {
 		if (!tmp[layer].canBuyMax) canMax = false;
 		let amt = player[layer].points.plus((canMax&&tmp[layer].baseAmount.gte(tmp[layer].nextAt))?tmp[layer].resetGain:0).div(tmp[layer].directMult);
 		let extraCost = Decimal.pow(tmp[layer].base, amt.pow(tmp[layer].exponent).div(tmp[layer].gainExp)).times(tmp[layer].gainMult);
 		let cost = extraCost.times(tmp[layer].requires).max(tmp[layer].requires);
 		if (tmp[layer].roundUpCost) cost = cost.ceil();
 		return cost;
-	} else if (type=="normal") {
+	} else if (type == "normal") {
 		let next = tmp[layer].resetGain.add(1).div(tmp[layer].directMult);
 		if (next.gte(tmp[layer].softcap)) next = next.div(tmp[layer].softcap.pow(decimalOne.sub(tmp[layer].softcapPower))).pow(decimalOne.div(tmp[layer].softcapPower));
 		next = next.root(tmp[layer].gainExp).div(tmp[layer].gainMult).root(tmp[layer].exponent).times(tmp[layer].requires).max(tmp[layer].requires);
 		if (tmp[layer].roundUpCost) next = next.ceil();
 		return next;
-	} else if (type=="custom") {
+	} else if (type == "custom") {
 		return layers[layer].getNextAt(canMax);
 	} else {
 		return decimalZero;
@@ -104,11 +104,11 @@ function shouldNotify(layer) {
 };
 
 function canReset(layer) {	
-	if (layers[layer].canReset!== undefined)
+	if (layers[layer].canReset !== undefined)
 		return run(layers[layer].canReset, layers[layer]);
 	else if (tmp[layer].type == "normal")
 		return tmp[layer].baseAmount.gte(tmp[layer].requires);
-	else if (tmp[layer].type== "static")
+	else if (tmp[layer].type == "static")
 		return tmp[layer].baseAmount.gte(tmp[layer].nextAt);
 	else 
 		return false;
