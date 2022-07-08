ea0877e56558ac6a3fa4b938cacdc029e2bcfafc
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-07-08 15:50:25 -0400

    update version

--- js/mod.js
+++ js/mod.js
@@ -9,8 +9,8 @@ let modInfo = {
 };
 
 let VERSION = {
-	num: '2.2',
-	name: 'Feats',
+	num: '2.3',
+	name: 'The Greeks',
 };
 
 let winText = '<h3>You won the game!</h3><br>However, it isn\'t the end yet...<br>Wait for more updates for further content.';
